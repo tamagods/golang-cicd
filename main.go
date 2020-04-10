4fb928e007a0f0358d395cbb6bb7a4464e95c44a
Bordin Intarama <tam@Bordins-MacBook-Pro.local>
2020-04-10 12:17:01 +0700

    Modified main.go

--- main.go
+++ main.go
@@ -8,9 +8,8 @@ import (
 
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
+	e.POST("/users", saveUser)
+	e.GET("/users/:id", getUser)
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
@@ -21,16 +20,8 @@ func getUser(c echo.Context) error {
 	return c.String(http.StatusOK, id)
 }
 
-//e.GET("/show", show)
-func show(c echo.Context) error {
-	// Get team and member from the query string
-	team := c.QueryParam("team")
-	member := c.QueryParam("member")
-	return c.String(http.StatusOK, "team:"+team+", member:"+member)
-}
-
 // e.POST("/save", save)
-func save(c echo.Context) error {
+func saveUser(c echo.Context) error {
 	// Get name and email
 	name := c.FormValue("name")
 	email := c.FormValue("email")
